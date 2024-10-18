146bfd13ea7989e55441906b62b86257149aa3e0
LiamSalazar <150866232+LiamSalazar@users.noreply.github.com>
2024-10-18 11:59:57 -0600

    Update RemoveElement.py

--- RemoveElement.py
+++ RemoveElement.py
@@ -1,14 +1,8 @@
-def removeElement(nums, val):
-    for element in nums:
-        if nums[element] == val:
-            nums.remove(element)
-    for i in range(0, element, 1):
-        nums.append('_')
-    k = len(nums) - (i+1)
-    print(k, ", nums = ", nums, sep='')
-
-
-
-nums = [0,1,2,2,3,0,4,2]
-val = 2
-removeElement(nums, val)
+class Solution(object):
+    def removeElement(self, nums, val):
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
