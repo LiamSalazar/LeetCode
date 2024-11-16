d2ea4cfa028e57a9fbd586497a0b2aa0c90d1bde
LiamSalazar <150866232+LiamSalazar@users.noreply.github.com>
2024-11-16 01:48:07 -0600

    Add files via upload

---
+++ 43.MultiplyStrings.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        numint1 = int(num1)
+        numint2 = int(num2)
+        result = numint1 * numint2
+        resultString = str(result)
+        return resultString
