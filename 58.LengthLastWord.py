51bd1dc586669a572f55236680589964e0319a77
LiamSalazar <150866232+LiamSalazar@users.noreply.github.com>
2024-11-19 13:44:38 -0600

    Update 58.LengthLastWord.py

--- 58.LengthLastWord.py
+++ 58.LengthLastWord.py
@@ -1,16 +1,14 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
         i = len(s)-1
-        length = 0 
+        count = 0 
         while i>=0 and s[i]==" ":
             i-=1
-
         while i>=0 and s[i]!=" ":
-            length+=1
+            count+=1
             i-=1
+        return count
 
-        return length
-        
 s = "a"
 sol = Solution()
 r = sol.lengthOfLastWord(s)
